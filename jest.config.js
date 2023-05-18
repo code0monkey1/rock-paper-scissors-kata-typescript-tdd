a8c63462f6ac600db60099e8f173fd1fd1facdcc
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-18 03:53:17 +0000

    18 May 2023, 09:23

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globals : {
-    "ts-jest": {
-      isolatedModules: true,
-    },
-  }
+  globals: {
+      'ts-jest': {
+        isolatedModules: true,
+        diagnostics: false,
+      },
+    }
 };
 
 
